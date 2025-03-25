862b8710b8e792799f535895d2360a490bf45b57
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-03-25 13:08:28 +0900

    Some small tweaks to the mod... the color will now display on the UI and will appear slightly grayer when not editable; the text can now still be shown through clouds when enabled shader; The text on the marker will now remain bright even enabled shader; the text has been converted to world space instead of camera space; exiting and closing the game will now save the renderOverlay options.

--- src/main/java/me/contaria/anglesnap/AngleSnap.java
+++ src/main/java/me/contaria/anglesnap/AngleSnap.java
@@ -3,6 +3,7 @@ package me.contaria.anglesnap;
 import com.mojang.logging.LogUtils;
 import me.contaria.anglesnap.config.AngleSnapConfig;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
@@ -14,9 +15,7 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.StickyKeyBinding;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Colors;
 import net.minecraft.util.Identifier;
@@ -29,14 +28,16 @@ import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 
+import java.awt.*;
 import java.util.Objects;
 
 public class AngleSnap implements ClientModInitializer {
     public static final Logger LOGGER = LogUtils.getLogger();
     public static final AngleSnapConfig CONFIG = new AngleSnapConfig();
-
+    private static double lastTickPosX,lastTickPosY,lastTickPosZ = 0;
     public static KeyBinding openMenu;
     public static KeyBinding openOverlay;
+    private static boolean wasToggleOverlayPressed = false;
 
     @Override
     public void onInitializeClient() {
@@ -45,14 +46,13 @@ public class AngleSnap implements ClientModInitializer {
                 GLFW.GLFW_KEY_F6,
                 "anglesnap.key"
         ));
-        openOverlay = KeyBindingHelper.registerKeyBinding(new StickyKeyBinding(
+        openOverlay = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "anglesnap.key.openoverlay",
                 GLFW.GLFW_KEY_F7,
-                "anglesnap.key",
-                () -> true
+                "anglesnap.key"
         ));
 
-        WorldRenderEvents.LAST.register(AngleSnap::renderOverlay);
+        WorldRenderEvents.LAST.register(ctx -> AngleSnap.renderOverlay(ctx.matrixStack()));
         HudLayerRegistrationCallback.EVENT.register(drawer -> drawer.attachLayerAfter(IdentifiedLayer.DEBUG, Identifier.of("anglesnap", "overlay"), AngleSnap::renderHud));
 
         ClientPlayConnectionEvents.JOIN.register((networkHandler, packetSender, client) -> {
@@ -63,10 +63,22 @@ public class AngleSnap implements ClientModInitializer {
             }
         });
         ClientPlayConnectionEvents.DISCONNECT.register((networkHandler, client) -> AngleSnap.CONFIG.unloadAngles());
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            boolean isKeyDown = openOverlay.isPressed();
+            if (isKeyDown && !wasToggleOverlayPressed) {
+                if (client.player != null) {
+                    AngleSnap.CONFIG.renderOverlays.setValue(!AngleSnap.CONFIG.renderOverlays.getValue());
+                    LOGGER.info("Overlay toggled: " + AngleSnap.CONFIG.renderOverlays.getValue());
+                    AngleSnap.CONFIG.save();
+                    AngleSnap.CONFIG.load();
+                }
+            }
+            wasToggleOverlayPressed = isKeyDown;
+        });
     }
 
     public static boolean shouldRenderOverlay() {
-        return openOverlay.isPressed();
+        return AngleSnap.CONFIG.renderOverlays.getValue();
     }
 
     private static void renderHud(DrawContext context, RenderTickCounter tickCounter) {
@@ -77,44 +89,64 @@ public class AngleSnap implements ClientModInitializer {
         }
     }
 
-    private static void renderOverlay(WorldRenderContext context) {
+    public static void renderOverlay(MatrixStack matrixStack) {
         if (shouldRenderOverlay()) {
             for (AngleEntry angle : AngleSnap.CONFIG.getAngles()) {
-                renderMarker(context, angle, AngleSnap.CONFIG.markerScale.getValue(), AngleSnap.CONFIG.textScale.getValue());
+                renderMarker(matrixStack, angle, AngleSnap.CONFIG.markerScale.getValue(), AngleSnap.CONFIG.textScale.getValue());
             }
         }
     }
 
-    private static void renderMarker(WorldRenderContext context, AngleEntry angle, float markerScale, float textScale) {
-        markerScale = markerScale / 10.0f;
-        textScale = textScale / 50.0f;
+    private static void renderMarker(MatrixStack matrixStack, AngleEntry angle, float markerScale, float textScale) {
+        markerScale = markerScale / 1.5f;
+        textScale = textScale / 4;
+        MinecraftClient client = MinecraftClient.getInstance();
 
-        Vector3f pos = Vec3d.fromPolar(
+        Vec3d camPos = client.cameraEntity.getEyePos();
+
+
+
+        Vec3d pos = Vec3d.fromPolar(
                 MathHelper.wrapDegrees(angle.pitch),
                 MathHelper.wrapDegrees(angle.yaw + 180.0f)
-        ).multiply(-1.0, 1.0, -1.0).toVector3f();
-        Quaternionf rotation = context.camera().getRotation();
+        ).multiply(-10.0, 10.0, -10.0);
+        Vec3d worldPos = camPos.add(pos.x, pos.y, pos.z);
+        Quaternionf rotation = MinecraftClient.getInstance().gameRenderer.getCamera().getRotation();
 
-        drawIcon(context, pos, rotation, angle, markerScale);
+        lastTickPosX = camPos.getX();
+        lastTickPosY = camPos.getY();
+        lastTickPosZ = camPos.getZ();
+
+        float x = (float) (worldPos.getX() - MathHelper.lerp(0, lastTickPosX, camPos.getX()));
+        float y = (float) (worldPos.getY() - MathHelper.lerp(0, lastTickPosY, camPos.getY()));
+        float z = (float) (worldPos.getZ() - MathHelper.lerp(0, lastTickPosZ, camPos.getZ()));
+
+        matrixStack.push();
+        matrixStack.translate(x, y, z);
+        drawIcon(matrixStack, pos, rotation, angle, markerScale);
         if (!angle.name.isEmpty()) {
-            drawName(context, pos, rotation, angle, textScale);
+            drawName(matrixStack,angle.color, pos, rotation, angle, textScale);
         }
+        matrixStack.pop();
     }
 
-    private static void drawIcon(WorldRenderContext context, Vector3f pos, Quaternionf rotation, AngleEntry angle, float scale) {
+    private static void drawIcon(MatrixStack matrixStack, Vec3d pos, Quaternionf rotation, AngleEntry angle, float scale) {
         if (scale == 0.0f) {
             return;
         }
 
-        MatrixStack matrices = Objects.requireNonNull(context.matrixStack());
+        MatrixStack matrices = Objects.requireNonNull(matrixStack);
         matrices.push();
-        matrices.translate(pos.x(), pos.y(), pos.z());
+        matrices.translate(pos.x, pos.y, pos.z);
         matrices.multiply(rotation);
         matrices.scale(scale, -scale, scale);
 
         Matrix4f matrix4f = matrices.peek().getPositionMatrix();
         RenderLayer layer = RenderLayer.getGuiTexturedOverlay(angle.getIcon());
-        VertexConsumer consumer = Objects.requireNonNull(context.consumers()).getBuffer(layer);
+
+        VertexConsumerProvider.Immediate immediate = getVertexConsumer();
+
+        VertexConsumer consumer = immediate.getBuffer(layer);
         consumer.vertex(matrix4f, -1.0f, -1.0f, 0.0f).color(angle.color).texture(0.0f, 0.0f);
         consumer.vertex(matrix4f, -1.0f, 1.0f, 0.0f).color(angle.color).texture(0.0f, 1.0f);
         consumer.vertex(matrix4f, 1.0f, 1.0f, 0.0f).color(angle.color).texture(1.0f, 1.0f);
@@ -123,25 +155,29 @@ public class AngleSnap implements ClientModInitializer {
         matrices.scale(1.0f / scale, 1.0f / -scale, 1.0f / scale);
         matrices.pop();
     }
+    private static VertexConsumerProvider.Immediate getVertexConsumer() {
+        return MinecraftClient.getInstance().getBufferBuilders().getEffectVertexConsumers();
+    }
 
-    private static void drawName(WorldRenderContext context, Vector3f pos, Quaternionf rotation, AngleEntry angle, float scale) {
+    private static void drawName(MatrixStack matrixStack, int markerColor, Vec3d pos, Quaternionf rotation, AngleEntry angle, float scale) {
         if (scale == 0.0f || angle.name.isEmpty()) {
             return;
         }
 
-        MatrixStack matrices = Objects.requireNonNull(context.matrixStack());
+        MatrixStack matrices = Objects.requireNonNull(matrixStack);
         matrices.push();
-        matrices.translate(pos.x(), pos.y(), pos.z());
+        matrices.translate(pos.x, pos.y, pos.z);
         matrices.multiply(rotation);
         matrices.scale(scale, -scale, scale);
 
         Matrix4f matrix4f = matrices.peek().getPositionMatrix();
         MinecraftClient client = MinecraftClient.getInstance();
+        VertexConsumerProvider.Immediate immediate = getVertexConsumer();
         TextRenderer textRenderer = client.textRenderer;
         float x = -textRenderer.getWidth(angle.name) / 2.0f;
         int backgroundColor = (int) (client.options.getTextBackgroundOpacity(0.25f) * 255.0f) << 24;
         textRenderer.draw(
-                angle.name, x, -15.0f, Colors.WHITE, false, matrix4f, context.consumers(), TextRenderer.TextLayerType.SEE_THROUGH, backgroundColor, 15
+                angle.name, x, -15.0f, markerColor, false, matrix4f, immediate, TextRenderer.TextLayerType.SEE_THROUGH, backgroundColor, LightmapTextureManager.MAX_LIGHT_COORDINATE
         );
 
         matrices.scale(1.0f / scale, 1.0f / -scale, 1.0f / scale);
